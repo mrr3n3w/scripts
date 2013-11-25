3ed0682db0390ea9196881560fec38ca0e41f0d2
mrr3n3w <Jordan@Jordans-MacBook-Pro.local>
2013-11-25 12:47:20 -0800

    removed the sleep kind of useless

--- backup.sh
+++ backup.sh
@@ -3,5 +3,4 @@ read -p "Please enter an email: " email
 
 tar -cf "home`/bin/date +%Y%m%d`".tar.gz ~
 uuencode "home`/bin/date +%Y%m%d`.tar.gz" unixdirectory | mailx -s "Unix Directory for `/bin/date +%Y%m%d`" $email
-sleep 10
 rm "home`/bin/date +%Y%m%d`.tar.gz"
